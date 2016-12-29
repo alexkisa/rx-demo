f1164759d5250ea47d161c4be00ebdb56c4c2273
Alex Kisa <alexkisa@gmail.com>
2016-12-29 14:58:13 +1100

    Create in-memory repository

---
+++ RxDemo/RxDemo.Tests/WhenUsingRepository.cs
@@ -0,0 +1,63 @@
+﻿using Xunit;
+
+namespace RxDemo.Tests
+{
+    public class WhenUsingRepository
+    {
+        [Fact]
+        public void ShouldReadNullWhenEmpty()
+        {
+            var repository = new Repository();
+            Assert.Null(repository.Read<Entity>(""));
+        }
+
+        [Fact]
+        public void ShouldUpdateNewEntityWhenEmpty()
+        {
+            var repository = new Repository();
+            repository.Update<Entity>("", Assert.NotNull);
+        }
+
+        [Fact]
+        public void ShouldReadUpdatedEntity()
+        {
+            var repository = new Repository();
+            repository.Update<Entity>("id", entity => entity.Property = "value");
+            Assert.Equal("value", repository.Read<Entity>("id").Property);
+        }
+
+        [Fact]
+        public void ShouldNotifyOnSubscription()
+        {
+            var repository = new Repository();
+
+            var notified = false;
+            repository.Subscribe()
+                .Do<Entity>((_, __) => notified = true);
+
+            repository.Update<Entity>("", entity => {});
+
+            Assert.True(notified);
+        }
+
+        [Fact]
+        public void ShouldFilterSubscriptionOnPredicate()
+        {
+            var repository = new Repository();
+
+            var notified = false;
+            repository.Subscribe()
+                .When<Entity>((_, __) => false)
+                .Do<Entity>((_, __) => notified = true);
+
+            repository.Update<Entity>("", entity => { });
+
+            Assert.False(notified);
+        }
+
+        public class Entity
+        {
+            public string Property { get; set; }
+        }
+    }
+}
---
+++ RxDemo/RxDemo/Repository.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RxDemo
+{
+    internal class Repository
+    {
+        private readonly Dictionary<Type, Dictionary<string, object>> _typeEntityLookup = new Dictionary<Type, Dictionary<string, object>>();
+        private readonly List<Subscription> _subscriptions = new List<Subscription>();
+
+        public T Read<T>(string id)
+        {
+            var entityLookup = ValueOrDefault(_typeEntityLookup, typeof(T));
+            if (entityLookup == null) return default(T);
+
+            return (T) ValueOrDefault(entityLookup, id);
+        }
+
+        public IEnumerable<T> ReadAll<T>()
+        {
+            var entityLookup = ValueOrDefault(_typeEntityLookup, typeof(T));
+            return entityLookup?.Select(kv => kv.Value).OfType<T>() ?? Enumerable.Empty<T>();
+        }
+
+        public void Update<T>(string id, Action<T> update) where T : class, new()
+        {
+            var entity = Read<T>(id) ?? new T();
+            update(entity);
+
+            var type = typeof(T);
+            if (!_typeEntityLookup.ContainsKey(type)) _typeEntityLookup[type] = new Dictionary<string, object>();
+            _typeEntityLookup[type][id] = entity;
+
+            _subscriptions.ForEach(s => s.TryNotify(id, entity));
+        }
+
+        public Subscription Subscribe()
+        {
+            var subscription = new Subscription();
+            _subscriptions.Add(subscription);
+            return subscription;
+        }
+
+        private static TValue ValueOrDefault<TKey, TValue>(IDictionary<TKey, TValue> dictionary, TKey key)
+        {
+            TValue value;
+            dictionary.TryGetValue(key, out value);
+            return value;
+        }
+
+        public class Subscription
+        {
+            private Func<string, object, bool> _predicate;
+            private Action<string, object> _action;
+
+            public Subscription When<T>(Func<string, T, bool> predicate) where T : class
+            {
+                _predicate = (id, obj) =>
+                {
+                    var entity = obj as T;
+                    return entity != null && predicate(id, entity);
+                };
+
+                return this;
+            }
+
+            public void Do<T>(Action<string, T> action) where T : class
+            {
+                _action = (id, obj) =>
+                {
+                    var entity = obj as T;
+                    if (entity != null) action(id, entity);
+                };
+            }
+
+            public void TryNotify<T>(string id, T entity)
+            {
+                if (_predicate == null || _predicate(id, entity)) _action?.Invoke(id, entity);
+            }
+        }
+    }
+}
